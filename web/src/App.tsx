1f9133ccb62b9b85ae691fc372a4498d235f3cbc
flavioflnlima <flavioflnlima@gmail.com>
2020-10-15 23:54:24 -0300

    iniciado o landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/Logo.svg'
+
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Paraíso do Tocantins</strong>
+          <span>Tocantins</span>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 

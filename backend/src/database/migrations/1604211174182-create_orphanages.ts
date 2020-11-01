27fa1e3162ae3b57650f669ac67e12909e460c4a
flavioflnlima <flavioflnlima@gmail.com>
2020-11-01 05:43:45 -0300

    correção da tabela orphanages

--- backend/src/database/migrations/1604211174182-create_orphanages.ts
+++ backend/src/database/migrations/1604211174182-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1604211174182 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
